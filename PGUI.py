804c5b7b237965e69832fc7b91db854fde3f4400
Reese <reesehe.66146@gmail.com>
2024-12-12 16:37:32 +0000

    Made naming convention PascalCase in class

--- PGUI.py
+++ PGUI.py
@@ -1,7 +1,7 @@
 # Python file containing classes to make instances of UI objects such as buttons
 """
 v 1.0
- - Normal and image static buttton class
+ - Normal and Image static buttton class
  - Normal static fonts class
 """
 import pygame as pg
@@ -16,32 +16,32 @@ DefaultFont = "freesansbold.ttf"
 
 # --- Text generator ---
 """
-Font class takes various arguments to create and instance of text and rectangle with their attributes
+Font class takes various arguments to create and instance of Text and rectangle with their attributes
 This simplifies the use of fonts throughout the game after creation
 """
 class TextGen:
-    def __init__(self, size, text, textColour, backgroundColour, centrePosX, centrePosY, antialias = True):
-        self.font = pg.font.Font(DefaultFont, size)
-        self.text = self.font.render(text, antialias, textColour, backgroundColour)
-        self.rect = self.text.get_rect()
-        self.rect.center = (centrePosX, centrePosY)
+    def __init__(self, Size, Text, TextColour, BGColour, MidPosX, MidPosY, Antialias = True):
+        self.font = pg.font.Font(DefaultFont, Size)
+        self.Text = self.font.render(Text, Antialias, TextColour, BGColour)
+        self.rect = self.Text.get_rect()
+        self.rect.center = (MidPosX, MidPosY)
         self.originX = self.rect[0]
         self.originY = self.rect[1]
         self.endX = self.originX + self.rect[2]
         self.endY = self.originY + self.rect[3]
 
-    # Renders the text onto the screen
+    # Renders the Text onto the screen
     def render(self, Window):
-        Window.blit(self.text, self.rect)
+        Window.blit(self.Text, self.rect)
 
-# --- Button text generator ---
+# --- Button Text generator ---
 # Works similarly to the font generator but has interactivity by checking whether there is input within its hit box
 class TextButton:
-    def __init__(self, size, text, antialias, defColour, centrePosX, centrePosY):
-        self.font = pg.font.Font(DefaultFont, size)
-        self.text = self.font.render(text, antialias, defColour[1], defColour[0])
-        self.rect = self.text.get_rect()
-        self.rect.center = (centrePosX, centrePosY)
+    def __init__(self, Size, Text, Antialias, defColour, MidPosX, MidPosY):
+        self.font = pg.font.Font(DefaultFont, Size)
+        self.Text = self.font.render(Text, Antialias, defColour[1], defColour[0])
+        self.rect = self.Text.get_rect()
+        self.rect.center = (MidPosX, MidPosY)
         self.originX = self.rect[0]
         self.originY = self.rect[1]
         self.endX = self.originX + self.rect[2]
@@ -49,7 +49,7 @@ class TextButton:
 
     # Renders the button onto the screen
     def render(self, Window):
-        Window.blit(self.text, self.rect)
+        Window.blit(self.Text, self.rect)
 
     # Checks whether the input is within the button parameters
     def positionCheck(self, MousePos):
@@ -57,15 +57,15 @@ class TextButton:
             return True
 
     def changeText(self, newText):
-        self.text = newText
+        self.Text = newText
 
-# --- Button image generator ---
-# Works similarly but text attributes are replaced with an image
+# --- Button Image generator ---
+# Works similarly but Text attributes are replaced with an Image
 class ImgButton:
-    def __init__(self, image, centrePosX, centrePosY):
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.center = (centrePosX, centrePosY)
+    def __init__(self, Image, MidPosX, MidPosY):
+        self.Image = Image
+        self.rect = self.Image.get_rect()
+        self.rect.center = (MidPosX, MidPosY)
         self.originX = self.rect[0]
         self.originY = self.rect[1]
         self.endX = self.originX + self.rect[2]
@@ -73,7 +73,7 @@ class ImgButton:
 
     # Renders the button onto the screen
     def render(self, Window):
-        Window.blit(self.image, self.rect)
+        Window.blit(self.Image, self.rect)
 
     # Checks whether the input is within the button parameters
     def positionCheck(self, MousePos):
